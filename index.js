d71ab550f12962ab613e3163989cc717dd8a57c6
Shunsuke Toba <boushi_sukaimu@yahoo.co.jp>
2018-06-20 14:41:41 +0900

    fix: entry

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-import verticalGutter from './src/components/vertical-gutter.vue'
-import horizontalGutter from './src/components/horizontal-gutter.vue'
 import columnGutter from './components/column-gutter.vue'
 import rowGutter from './components/row-gutter.vue'
 
-export { verticalGutter, horizontalGutter, columnGutter, rowGutter }
+export { columnGutter, rowGutter }
