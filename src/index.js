1a1b83efa827fcf364516e4e61393940bd298950
Shunsuke Toba <boushi_sukaimu@yahoo.co.jp>
2018-06-23 00:50:18 +0900

    feat: register vue component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
+import Vue from 'vue'
 import columnGutter from './components/column-gutter.vue'
 import rowGutter from './components/row-gutter.vue'
 
-export {
+const Components = {
   columnGutter, 
   rowGutter 
 }
+
+Object.keys(Components).forEach(name => {
+  Vue.component(name, Components[name])
+})
+
+export default Components
